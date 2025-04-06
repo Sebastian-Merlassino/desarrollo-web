913ac0d507d70dda4eb4e4fb386fcc5fddee594e
Sebastian Merlassino <sebastianmerlassino@gmail.com>
2025-04-06 12:23:24 -0300

    pueba 1 de AOS

---
+++ main.js
@@ -0,0 +1,22 @@
+
+let suma = 10 + 15; // 15
+let producto = 20 * 2; // 40
+
+
+
+if (suma > 20) {
+    console.log("La suma es mayor que 20");
+  }
+  else {
+    console.log("La suma es menor o igual a 20");
+  }
+
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+
+  let i = 0;
+while (i < 7) {
+  console.log(i);
+  i++;
+}
