70c7001dc3918f4eded960b4e252dec6bba35be3
Sebastian-Merlassino <168592380+Sebastian-Merlassino@users.noreply.github.com>
2025-04-09 00:06:31 +0000

    actualización de header y servicios de inicio

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 
-let suma = 10 + 15; // 15
-let producto = 20 * 2; // 40
+// let suma = 10 + 15; // 15
+// let producto = 20 * 2; // 40
 
 
 
-if (suma > 20) {
-    console.log("La suma es mayor que 20");
-  }
-  else {
-    console.log("La suma es menor o igual a 20");
-  }
+// if (suma > 20) {
+//     console.log("La suma es mayor que 20");
+//   }
+//   else {
+//     console.log("La suma es menor o igual a 20");
+//   }
 
-  for (let i = 0; i < 5; i++) {
-    console.log(i);
-  }
+//   for (let i = 0; i < 5; i++) {
+//     console.log(i);
+//   }
 
-  let i = 0;
-while (i < 7) {
-  console.log(i);
-  i++;
-}
+//   let i = 0;
+// while (i < 7) {
+//   console.log(i);
+//   i++;
+// }
 
 
 // CLASE 1 //
@@ -139,3 +139,6 @@ while (i < 7) {
 //     contador++
     
 // }
+
+// CLASE 2
+
